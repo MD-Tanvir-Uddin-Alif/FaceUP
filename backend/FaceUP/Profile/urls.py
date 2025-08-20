d7327cce726cf53cbcf5c4f10402f1ff07990433
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-08-20 20:45:28 +0600

    User Lgin(JWT) implmented

--- backend/FaceUP/Profile/urls.py
+++ backend/FaceUP/Profile/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import UserProfileCreateView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     path('registration/',UserProfileCreateView.as_view(), name="user_registration"),
+    path('login/', TokenObtainPairView.as_view(), name="user_login"),
 ]
