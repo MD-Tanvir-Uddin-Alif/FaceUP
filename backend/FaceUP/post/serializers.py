86a02b98b593e565f03b89f25ada2b4041945214
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-08-23 14:19:38 +0600

    Comment serializer is created

--- backend/FaceUP/post/serializers.py
+++ backend/FaceUP/post/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import PostModel
+from .models import PostModel, CommentModel
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,3 +7,12 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostModel
         fields = ['id', 'author', 'content', 'image', 'created_at', 'updated_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    
+    class Meta:
+        model = CommentModel
+        fields = ['id', 'post', 'author', 'created_at']
+        read_only_fields = ['author', 'created_at']
