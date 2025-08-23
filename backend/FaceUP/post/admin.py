dbb4d834491f6367973d41b34e0dbeb78d3b189f
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-08-23 17:31:51 +0600

    Like model created and registered to admin panel

--- backend/FaceUP/post/admin.py
+++ backend/FaceUP/post/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import PostModel, CommentModel
+from .models import PostModel, CommentModel,LikeModel
 # Register your models here.
 
 
@@ -13,3 +13,9 @@ class PostModelAdmin(admin.ModelAdmin):
 class CommentModelAdmin(admin.ModelAdmin):
     model = CommentModel
     list_display = ('id', 'post', 'author', 'created_at')
+
+
+@admin.register(LikeModel)
+class LikeModelAdmin(admin.ModelAdmin):
+    model = LikeModel
+    list_display = ('id', 'user', 'post', 'created_at')
