acd25823f8cea862a6d4615524ab1ce7b50b2ad9
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-08-24 17:59:55 +0600

    Forntend Initialised

---
+++ frontend/FaceUP/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='bg-black text-red-600'>Forntend Initialised</h1>
+    </div>
+  )
+}
+
+export default App
