0cc471b03d598129bc4223fb83d4b3ff73b78d79
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-08-26 18:05:09 +0600

    navbar profile and logtout design created

--- frontend/FaceUP/src/assets/components/Navbar.jsx
+++ frontend/FaceUP/src/assets/components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+    const [userProfile, setUserProfile] = useState(null);
+    const [isLogin, setIsLogin] = useState(!!localStorage.getItem('accessToken'));
+    useEffect(() => {
+    const updateLoginStatus = () => {
+      setIsLogin(!!localStorage.getItem('accessToken'));
+
+      if (!localStorage.getItem('accessToken')) {
+        setUserProfile(null);
+      }
+    };
+
+
+    window.addEventListener('loginStatusChanged', updateLoginStatus);
+
+    return () => {
+      window.removeEventListener('loginStatusChanged', updateLoginStatus);
+    };
+  }, []);
+
+
+
   return (
     <div>
         <nav class="bg-white shadow sticky top-0 z-10">
@@ -17,6 +39,34 @@ const Navbar = () => {
                 </div>
 
                 <div>
+                {!isLogin?(
+                    <Link to='user/login/'>
+                    <button class="rounded-md border border-black px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white transition">
+                    Login
+                    </button>
+                </Link>
+                ):(
+                    <div className="relative inline-block text-left">
+                    <input type="checkbox" id="dropdownToggle" className="hidden peer" />
+                    <label htmlFor="dropdownToggle" className="cursor-pointer">
+                <img
+                  src={getProfileImage()}
+                  alt="Profile"
+                  className="w-14 h-14 rounded-full border border-black object-cover"
+                  onError={(e) => {
+                    e.target.src = 'https://via.placeholder.com/60';
+                  }}
+                />
+              </label>
+
+              {/* <div className="dropdown-menu hidden peer-checked:block absolute right-0 mt-2 w-40 bg-white border border-black rounded-lg shadow-md z-10">
+                <Link to='/profile'>
+                <button className="w-full text-left block px-4 py-2 text-sm hover:bg-black hover:text-white transition">Profile</button>
+                </Link>
+                <button onClick={handleLogout} className="w-full text-left block px-4 py-2 text-sm hover:bg-black hover:text-white transition">Logout</button>
+              </div> */}
+            </div>
+                )}
                 <Link to='user/login/'>
                     <button class="rounded-md border border-black px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white transition">
                     Login
